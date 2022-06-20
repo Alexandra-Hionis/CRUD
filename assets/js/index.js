5c4ce24fe7e1f85487a9d754ac5b20712718c921
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2022-06-20 16:54:38 -0400

    added creatPost function to apend to innerhtml and create new post

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,12 +3,41 @@ let newTodo = document.getElementById("newTodo");
 let msg = document.getElementById("msg");
 let toDoPosts = document.getElementById("toDoPosts");
 
+// let formValidation = () => {
+//   if (newTodo.value === "") {
+//     msg.innerHTML = "Post cannot be blank";
+//     console.log("failure");
+//   } else {
+//     console.log("successs");
+//     msg.innerHTML = "";
+//   }
+// };
+
+let data = {};
+
 let formValidation = () => {
   if (newTodo.value === "") {
     msg.innerHTML = "Post cannot be blank";
-    console.log("failure");
   } else {
-    console.log("successs");
-    msg.innerHTML = "";
+    // Other codes are here
+    acceptData();
   }
+};
+
+let createPost = () => {
+  toDoPosts.innerHTML += `
+    <div class="grid__style container"><p>${data.text}</p>
+    <span class="options">
+        <i onClick="editPost(this)" class="fas fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+      </span>
+    </div>
+    `;
+  newTodo.value = "";
+};
+
+let acceptData = () => {
+  data["text"] = newTodo.value;
+
+  createPost();
 };
